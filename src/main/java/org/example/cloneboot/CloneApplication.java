36fd627f16d19143b9ea2b1cc63b2a0e48278acf
ciny <ciy5364612@gmail.com>
2024-05-23 23:36:43 +0900

    CloneApplication 생성자 정의 및 빈 run 실행 메소드 구현

--- src/main/java/org/example/cloneboot/CloneApplication.java
+++ src/main/java/org/example/cloneboot/CloneApplication.java
@@ -1,6 +1,37 @@
 package org.example.cloneboot;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.Assert;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class CloneApplication {
-    public static void run(Class<?> primarySource, String... args){
+    private final ResourceLoader resourceLoader;
+    private final Set<Class<?>> primarySources;
+
+    public CloneApplication(Class<?>... primarySources) {
+        this(null, primarySources);
+    }
+
+    public CloneApplication(ResourceLoader resourceLoader, Class<?>... primarySources) {
+        this.resourceLoader = resourceLoader;
+        Assert.notNull(primarySources, "PrimarySources must not be null");
+        this.primarySources = new LinkedHashSet<>(Arrays.asList(primarySources));
+    }
+
+    public static ConfigurableApplicationContext run(String... args) {
+        return new GenericApplicationContext();
+    }
+
+    public static ConfigurableApplicationContext run(Class<?>[] primarySources, String... args) {
+        return new CloneApplication(primarySources).run(args);
+    }
+
+    public static ConfigurableApplicationContext run(Class<?> primarySource, String... args) {
+        return run(new Class<?>[]{primarySource}, args);
     }
 }
